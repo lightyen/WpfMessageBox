163c6dabd62ff53e60609b250a27609534b1885c
lightyen <lightyen0123@gmail.com>
2018-08-08 08:58:29 +0800

    test

---
+++ WpfMessageBox/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMessageBox {
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var result = NativeAPI.MessageBox(IntPtr.Zero, "hello world", "Test", MessageBoxFlag.YesNo | MessageBoxFlag.IconImformation);
+
+            switch (result) {
+                case MessageBoxReturn.Yes:
+                    textBlock1.Text = "Yes";
+                    break;
+                case MessageBoxReturn.No:
+                    textBlock1.Text = "No";
+                    break;
+                default:
+                    textBlock1.Text = "";
+                    break;
+            }
+        }
+    }
+}
---
+++ WpfMessageBox/NativeAPI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace WpfMessageBox {
+
+    //https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-messagebox
+
+    public static class NativeAPI {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern MessageBoxReturn MessageBox(IntPtr hWnd, string text, string caption, MessageBoxFlag type);
+    }
+
+    [Flags]
+    public enum MessageBoxFlag {
+        Ok = 0x00000000,
+        OkCancel = 0x00000001,
+        AbortRetryIgnore = 0x00000002,
+        YesNoCancel = 0x00000003,
+        YesNo = 0x00000004,
+        RetryCancel = 0x00000005,
+        CancelTryContinue = 0x00000006,
+        Help = 0x00004000,
+        IconExclamation = 0x00000030,
+        IconWarning = 0x00000030,
+        IconImformation = 0x00000040,
+        IconQuestion = 0x00000020,
+        IconStop = 0x00000010,
+        IconError = 0x00000010,
+        IconHand = 0x00000010,
+        // 還有更多選項, 但是我懶了...
+    }
+
+    [Flags]
+    public enum MessageBoxReturn {
+        Abort = 3,
+        Cancel = 2,
+        Continue = 11,
+        Ignore = 5,
+        No = 7,
+        Ok = 1,
+        Retry = 4,
+        TryAgain = 10,
+        Yes = 6,
+    }
+}
